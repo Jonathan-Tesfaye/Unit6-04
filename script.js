d959d8abd7c41809beebbe0d13c7c8ecb8cb6d5e
JonathanTesfaye <jonathan.tesfaye@ocsbstudent.ca>
2021-02-17 15:45:11 +0000

    Code changes

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
+// These are the variables going to be used
 let a = 0
 let b = 0
 let c = 0
+let answer = 0
 
-document.getElementById ('button').addEventListener ('click', formula)
+document.getElementById ('button').addEventListener('click', formula)
 
 function formula () {
-
+// I am linking the variables to the input
   a = document.getElementById('a-value').value
-  
   b = document.getElementById('b-value').value
-
   c = document.getElementById('c-value').value
-
+// I am making a string for the variables to be numbers
   a = parseInt(a)
-
   b = parseInt(b)
-
   c = parseInt(c)
-
-  button = (a + b)/2 * c
-
-  alert(button)
+// This is the formula to find the area of the trapezoid
+  answer = (a + b) / 2 * c
+  document.getElementById('answers').innerHTML = answer
 }
